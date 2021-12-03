60be17ce59f5d76fc0e81b69a7bf3c9c7e96be0a
Harsha Vardhan <94678080+Harsha6303709488@users.noreply.github.com>
2021-12-03 09:23:04 +0000

    Completed

--- p0final.c
+++ p0final.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+int main()
+{
+  char a[50];
+  printf ("my name is Harsha\n");
+  printf ("my id i 13713");
+return 0;
+}
---
+++ p2original.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int s,a,b
+int input()
+{
+  printf("enter two number\n");
+  scanf("%d%d",a,b);
+return 0
+}
+int add()
+{
+  int sum=a+b;
+  return 0;
+}
+void output()
+{
+  printf("%d", s);
+}
+void main()
+{
+  input();
+  s=add(a,b);
+output();
+}
